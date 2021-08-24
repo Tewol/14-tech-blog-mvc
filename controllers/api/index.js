667ab076e57e0b8aa0f7b8a05648f35bfdd8deb6
Hewan Redie <tewol22@gmail.com>
2021-08-24 06:09:59 -0500

    routes for user dashbord and home updated

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blogRoutes');
-
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 router.use('/users', userRoutes);
-router.use('/blogs', blogRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
