d2a30cf16dbbc23f62766b40aaaca02f03d6f55a
Hewan Redie <tewol22@gmail.com>
2021-08-24 21:49:06 -0500

    fixed blogs

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,17 +2,17 @@ const { Comment } = require("../models");
 
 const commentData = [
   {
-    comment_text: "Comment1",
+    comment_text: "Test comment",
     user_id: 1,
     post_id: 3,
   },
   {
-    comment_text: "Comment2",
+    comment_text: "Teststing 123",
     user_id: 3,
     post_id: 2,
   },
   {
-    comment_text: "Comment13",
+    comment_text: "Test1",
     user_id: 2,
     post_id: 1,
   },
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,19 +2,19 @@ const { Post } = require("../models");
 
 const postData = [
   {
-    title: "AI",
+    title: "A New Chip Cluster Will Make Massive AI Models Possible",
     content:
-      "Test1.",
+      "WHEN IT COMES to the neural networks that power today’s artificial intelligence, sometimes the bigger they are, the smarter they are too. Recent leaps in machine understanding of language, for example, have hinged on building some of the most enormous AI models ever and stuffing them with huge gobs of text. A new cluster of computer chips could now help these networks grow to almost unimaginable size—and show whether going ever larger may unlock further AI advances, not only in language understanding, but perhaps also in areas like robotics and computer vision.",
     user_id: 1,
   },
   {
-    title: "IOT",
-    content: "Test2.",
+    title: "Can Robots Evolve Into Machines of Loving Grace?",
+    content: "NOBODY COULD SAY exactly when the robots arrived. They seemed to have been smuggled onto campus during the break without any official announcement, explanation, or warning. There were a few dozen of them in total: six-wheeled, ice-chest-sized boxes with little yellow flags on top for visibility. They navigated the sidewalks around campus using cameras, radar, and ultrasonic sensors. They were there for the students, ferrying deliveries ordered via an app from university food services, but everyone I knew who worked on campus had some anecdote about their first encounter.",
     user_id: 2,
   },
   {
-    title: "Java",
-    content: "Test3.",
+    title: "Tesla Promised a Robot. Was It Just a Recruiting Pitch?",
+    content: "THE TESLA RECRUITMENT presentation ended Thursday night when a person in a tight white suit, their head encased in black, robot-walked onto the dark stage. Techno music blared, and the person began to Charleston. The person did the running man. The person waggled their shoulders and extended their arms into jazz hands. “All right. Thank you,” CEO Elon Musk said, an eternal-seeming 40 seconds into the freestyle, cutting off the dance.",
     user_id: 1,
   },
 ];
